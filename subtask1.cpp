2e84e13c5065a87984608770ea14e67884194625
Navneet Jindal <navneetjindal444@gmail.com>
2021-02-22 23:51:41 +0530

    Subtask1-completed

--- subtask1.cpp
+++ subtask1.cpp
@@ -1,122 +1,180 @@
 #include "opencv2/opencv.hpp"
 #include <iostream>
+#include <stdexcept>
 
 using namespace std;
 using namespace cv;
+// Comparator Function for sorting of Source points( As Clicked By User)
 bool comp(pair<int,int> x,pair<int,int> y)
 {
-     if(x.second<y.second)
-          return true;
-     if(x.second == y.second)
-     {
-          return x.first<y.first;
-     }
-     return false;
+    if(x.second<y.second)
+        return true;
+    if(x.second == y.second)
+    {
+        return x.first<y.first;
+    }
+    return false;
 }
+
+// Global Vectors for storing points
 vector<Point2f> source_pts,destination_pts;
 vector<pair<int,int>> source_pts_temp,destination_pts_temp;
+
+// Global Variable for storing number of times left mouse button is clicked on the displayed image
 int number_of_mouse_clicks = 0;
+
+// Processing Clicked Points
 void CLICKDETECTION(int event, int x, int y, int flags, void* userdata)
 {
-     if  ( event == EVENT_LBUTTONDOWN )
-     {
-          source_pts_temp.push_back(make_pair(x,y));
-          number_of_mouse_clicks++;
-     }
+    if( event == EVENT_LBUTTONDOWN and number_of_mouse_clicks < 4 )
+    {
+        source_pts_temp.push_back(make_pair(x,y));
+        number_of_mouse_clicks++;
+        if(number_of_mouse_clicks == 4)
+        {
+            destroyWindow("Original Frame");
+        }
+    }
      
 }
-void destination_points (int a , int b) {
-     for (int i = 0; i<4 ; i++){
-          destination_pts_temp[i].first = destination_pts_temp[i].first*a/1920;
-          destination_pts_temp[i].second = destination_pts_temp[i].second*b/1080;
-          int x = destination_pts_temp[i].first;
-          int y = destination_pts_temp[i].second;
-          destination_pts.push_back(Point2f(x,y));
-     }
-     
 
+// AutoScaling Destinantion Points according to size of image
+/*
+    We used Riju Ma'am destination point coordinates. But if original image has small size then there could be a issue of 
+    going out of boundary. So to accomodate that we scaled the destination points using the normal image size as 1920x1080. 
+*/
+void AutoScaling (int a , int b) {
+    for (int i = 0; i<4 ; i++){
+        destination_pts_temp[i].first = destination_pts_temp[i].first*a/1920;
+        destination_pts_temp[i].second = destination_pts_temp[i].second*b/1080;
+        int x = destination_pts_temp[i].first;
+        int y = destination_pts_temp[i].second;
+        destination_pts.push_back(Point2f(x,y));
+    } 
 }
+
+
 int main(int argc, char** argv)
 {
-     // Read image from file 
-     Mat img = imread("traffic.jpg", IMREAD_GRAYSCALE);
 
-     //if fail to read the image
-     if ( !img.data ) 
-     { 
-          cout <<  "Image not found or unable to open" << endl;
-          return -1; 
-     }
-     cout<<source_pts.size()<<endl;
-     //Create a window
-     namedWindow("Original Image",  WINDOW_NORMAL);
-     cv::setWindowProperty("Original Image",WND_PROP_FULLSCREEN, WINDOW_FULLSCREEN);
-
-     //set the callback function for any mouse event
-     setMouseCallback("Original Image", CLICKDETECTION, NULL);
-
-     //show the image
-     imshow("Original Image", img);
-     int width_of_inputimage = img.cols; // x coordinate
-     int height_of_inputimage=img.rows;  // y coordinate
-     destination_pts_temp = {make_pair(472,52),make_pair(800,52),make_pair(472,830),make_pair(800,830)};
-
-
-     // Wait until user press some key
-     while(1){
-          waitKey(1);
-          if(number_of_mouse_clicks == 4) break;
-     }
-     cout<<source_pts.size()<<endl;
-     destroyWindow("Original Image");
-     
-     cout<<"DONE 4 times"<<endl;
-     for( auto [a,b]:source_pts)
+     if(argc < 2)
      {
-          cout<<a<<" "<<b<<endl;
+          cout<<"Please specify a File name in the format : ./a.out $(filename) or"<<endl;
+        cout<<"To Compile and Execute Type Command : make all file=$(filename)\nTo Compile Type Command : make compile\nTo Execute Type Command : make run file=$(filename)"<<endl;
+        throw std::invalid_argument( "Wrong Command Line Argument");
+          return -1;
      }
 
-     sort(source_pts_temp.begin(),source_pts_temp.end(),comp);
-     sort(source_pts_temp.begin(),source_pts_temp.begin()+2);
-     sort(source_pts_temp.begin()+2,source_pts_temp.end());
-     for(int i = 0; i< 4; i++)
+     if(argc > 2)
      {
-          source_pts.push_back(Point2f(source_pts_temp[i].first,source_pts_temp[i].second));
-
+          cout<<"Too Many Arguments. Enter a single Filename"<<endl;
+        cout<<"To Execute Type Command : ./a.out $(filename) or"<<endl;
+          cout<<"To Compile and Execute Type Command : make all file=$(filename)\nTo Compile Type Command : make compile\nTo Execute Type Command : make run file=$(filename)"<<endl;
+        throw std::invalid_argument( "Wrong Command Line Argument");
+        return -1;
      }
 
+    // Reading Image
+     Mat img = imread(argv[1], IMREAD_GRAYSCALE);
 
-     /*
-          x*y
-          (472*x/1920,52*y/1080)
-     */
-     // Setting destination points 
-     destination_points(width_of_inputimage,height_of_inputimage);
-     
+    // Error Reading Image
+    if ( !img.data ) 
+    { 
+        throw std::invalid_argument( "Image File not found or unable to open");
+        return -1; 
+    }
 
-     Mat homography = findHomography(source_pts,destination_pts);
-     Mat projected_image;
-     warpPerspective(img,projected_image,homography,img.size());
-     imwrite("Projected Frame.jpeg",projected_image);
-     namedWindow("OUTPUT IMAGE", WINDOW_AUTOSIZE);
-     
-     imshow("OUTPUT IMAGE",projected_image);
-     waitKey(0);
-     destroyWindow("OUTPUT IMAGE");
-     
-     Rect cropped_coordinates;
-     cropped_coordinates.x = destination_pts_temp[0].first;
-     cropped_coordinates.y = destination_pts_temp[0].second;
-     cropped_coordinates.width = destination_pts_temp[1].first - destination_pts_temp[0].first;
-     cropped_coordinates.height = destination_pts_temp[2].second - destination_pts_temp[0].second;
+    // Creating a Window Frame
+    namedWindow("Original Frame",  WINDOW_AUTOSIZE);
+    
+    // Detecting a Mouse Click
+    setMouseCallback("Original Frame", CLICKDETECTION, NULL);
 
-     Mat crop = projected_image(cropped_coordinates);
-     imwrite("cropped_image.jpg",crop);
-     namedWindow("CROPPED IMAGE", WINDOW_AUTOSIZE);
-     imshow("CROPPED IMAGE",crop);
-     waitKey(0);
-     destroyWindow("CROPPED IMAGE");
-     
-     return 0;
+    // Showing the Image Frame
+    imshow("Original Frame", img);
 
+    // Storing size of image for Autoscaling
+    int width_of_inputimage = img.cols; // x coordinate
+    int height_of_inputimage=img.rows;  // y coordinate
+
+    // Storing Points in the order (Top Left,Top Right,Bottom Left,Bottom Right)
+    destination_pts_temp = {make_pair(472,52),make_pair(800,52),make_pair(472,830),make_pair(800,830)};
+
+    /*
+        Automatially Closing the image frame when user clicks 4 points(handled in CLICKDETECTION function).
+        If 4 clicks not detected on the frame and the frame is closed or a key is pressed then raising an exception.
+
+    */
+    waitKey(0);
+    if(cv::getWindowProperty("Original Frame",cv::WND_PROP_VISIBLE)!=1 and number_of_mouse_clicks < 4)
+    {
+        throw std::invalid_argument( "Image Closed before selecting 4 points");
+        return -1;
+    }
+     
+    // Ordering the Points Clicked by the user according to (Top Left,Top Right,Bottom Left,Bottom Right)
+    sort(source_pts_temp.begin(),source_pts_temp.end(),comp);
+    sort(source_pts_temp.begin(),source_pts_temp.begin()+2);
+    sort(source_pts_temp.begin()+2,source_pts_temp.end());
+    
+    // Storing Source points in the OpenCV coordinates format
+    for(int i = 0; i< 4; i++)
+    {
+        source_pts.push_back(Point2f(source_pts_temp[i].first,source_pts_temp[i].second));
+    }
+
+    //Calling the autoscaling Function
+    AutoScaling(width_of_inputimage,height_of_inputimage);
+     
+    // Finding the homography matrix for given 2 set of points
+    Mat homography = findHomography(source_pts,destination_pts);
+
+    // Defining Matrix for the projected image
+    Mat projected_image;
+
+    // Correcting the Camera Angle based on the found Homography Matrix
+    warpPerspective(img,projected_image,homography,img.size());
+
+    // Writing and Saving the Image
+    imwrite("Projected Frame.jpg",projected_image);
+
+    // Creating a Window Frame
+    namedWindow("Projected Image", WINDOW_AUTOSIZE);
+    
+    // Showing the Projected Image 
+    imshow("Projected Image",projected_image);
+
+    waitKey(0);
+
+    // Destroying any Existing Window
+    destroyAllWindows();
+    
+    // Defining the Cropped Coordinates for Cropping the Image
+    Rect cropped_coordinates;
+
+    //Setting the Top Left Coordinate from where the Image will be Cropped
+    cropped_coordinates.x = destination_pts_temp[0].first;
+    cropped_coordinates.y = destination_pts_temp[0].second;
+
+    // Size of the Cropped Image
+    cropped_coordinates.width = destination_pts_temp[1].first - destination_pts_temp[0].first;
+    cropped_coordinates.height = destination_pts_temp[2].second - destination_pts_temp[0].second;
+
+    // Forming the Matrix for Cropped Image 
+    Mat crop = projected_image(cropped_coordinates);
+
+    // Writing and Saving the Cropped Image
+    imwrite("Cropped Image.jpg",crop);
+
+    //Creating a Window
+    namedWindow("CROPPED IMAGE", WINDOW_AUTOSIZE);
+
+    // Showing the Cropped Image
+    imshow("CROPPED IMAGE",crop);
+    waitKey(0);
+
+    // Destroying any Existing Window
+    destroyAllWindows();
+    
+    return 0;
 }
